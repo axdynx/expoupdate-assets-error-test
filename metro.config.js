a880784d83be655fbb86e954790a388fa8b7e7ce
Rodriguez Lilian <l.rodriguez@d-i-n.fr>
2025-10-03 09:36:34 +0200

    Initial commit 3

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,6 @@
-// This replaces `const { getDefaultConfig } = require('expo/metro-config');`
-const { getSentryExpoConfig } = require('@sentry/react-native/metro');
+const { getDefaultConfig } = require('expo/metro-config');
 
-// This replaces `const config = getDefaultConfig(__dirname);`
-const config = getSentryExpoConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
 config.resolver.assetExts.push(
   // Adds support for `.html` files
